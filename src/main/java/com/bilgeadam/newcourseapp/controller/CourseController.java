12e36fb7221225f7d27332a0af8b6b1ea72668e9
Bora Yüret <borayuret@gmail.com>
2022-01-22 13:34:29 +0300

    CourseNotFound eklendi.

--- src/main/java/com/bilgeadam/newcourseapp/controller/CourseController.java
+++ src/main/java/com/bilgeadam/newcourseapp/controller/CourseController.java
@@ -1,13 +1,12 @@
 package com.bilgeadam.newcourseapp.controller;
 
 import com.bilgeadam.newcourseapp.entity.Course;
+import com.bilgeadam.newcourseapp.exception.CourseNotFound;
 import com.bilgeadam.newcourseapp.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -27,10 +26,28 @@ public class CourseController {
         if(courseDB.isPresent())
         {
             course = courseDB.get();
+
+            return course;
+        }
+        else
+        {
+            throw new CourseNotFound("Course not found with id:"+id);
         }
 
-        return course;
 
+    }
+
+    @GetMapping("/course")
+    public List<Course> getCourses()
+    {
+        return courseRepository.findAll();
+    }
+
+
+    @PostMapping("/course")
+    public Course saveCourse(@RequestBody Course course)
+    {
+        return courseRepository.save(course);
     }
 
 
